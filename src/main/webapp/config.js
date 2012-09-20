004cee3e530c538c6800dd4293a53b2044c336dc
Gethin James <Gethin.James@alfresco.com>
2012-09-20 10:13:08 +0100

    Changed to use a config.js file

---
+++ src/main/webapp/config.js
@@ -0,0 +1,8 @@
+var config = {
+  client_id:"your_id",
+  client_secret:"your_scope",
+  redirect_uri:"your_callback_url",
+  authURL:"https://stagapi.alfresco.com/auth/oauth/versions/2/authorize",
+  tokenURL:"https://stagapi.alfresco.com/auth/oauth/versions/2/token",
+  scope:"public_api"
+};
